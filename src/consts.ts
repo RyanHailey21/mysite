cb69a346d60fe9a769299df638c914101b0303eb
RyanHailey21 <ryanhaileygolf@gmail.com>
2025-03-07 08:01:34 -0600

    Initial commit: Personal portfolio website with improved color contrast

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My Personal Portfolio & Blog';
+export const SITE_DESCRIPTION = 'Welcome to my personal portfolio and blog where I share my projects and thoughts.';
